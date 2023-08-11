3a69c06965fdeb9f0ab69652c4d1b39155a1f90e
sirpheranme <sirpheranme@gmail.com>
2023-08-11 05:12:28 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+    char d;
+
+    for (d = '0'; d <= '9'; d++)
+    {
+        putchar(d);
+    }
+
+    for (d = 'a'; d <= 'f'; d++)
+    {
+        putchar(d);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
+
