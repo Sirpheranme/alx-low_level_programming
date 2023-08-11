42747c3a76157a6a3aa60e5ba9045e6042574517
sirpheranme <sirpheranme@gmail.com>
2023-08-11 04:58:59 -0700

    Numbers on line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+    int r;
+    r = 0;
+
+    while (r < 10)
+    {
+        putchar(r + '0');
+        r++;
+    }
+
+    putchar('\n');
+
+    return 0;
