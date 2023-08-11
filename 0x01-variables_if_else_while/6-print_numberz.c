f9ccf78ecb45c414dbd26b16f075bf20520572a3
sirpheranme <sirpheranme@gmail.com>
2023-08-11 05:01:29 -0700

    Numbers on line

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,15 +6,27 @@
 
 int main(void)
 {
-    int r;
-    r = 0;
-
-    while (r < 10)
-    {
-        putchar(r + '0');
-        r++;
-    }
-
+    putchar('0' + 0);
+    putchar(',');
+    putchar('0' + 1);
+    putchar(',');
+    putchar('0' + 2);
+    putchar(',');
+    putchar('0' + 3);
+    putchar(',');
+    putchar('0' + 4);
+    putchar(',');
+    putchar('0' + 5);
+    putchar(',');
+    putchar('0' + 6);
+    putchar(',');
+    putchar('0' + 7);
+    putchar(',');
+    putchar('0' + 8);
+    putchar(',');
+    putchar('0' + 9);
     putchar('\n');
 
     return 0;
+}
+
