0f512aa7a8af69a349f2c97ddbaf47f51df9101a
sirpheranme <sirpheranme@gmail.com>
2023-08-15 11:28:12 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,20 @@
 void jack_bauer(void)
 {
 	int i, j;
+
 	i = 0;
+
 	while (i < 24)
 	{
 		j = 0;
 		while (j < 60)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(':');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar('\n');
 			j++;
 		}
 		i++;
