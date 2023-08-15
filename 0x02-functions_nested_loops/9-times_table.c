4f3c375d6e26b43192f1ee803fb76d1584ac9cca
sirpheranme <sirpheranme@gmail.com>
2023-08-15 11:34:07 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 
 int main(void)
 {
-    times_table(); // Call the function to print the 9 times table
+    times_table(); /* Call the function to print the 9 times table */
     return 0;
 }
 
