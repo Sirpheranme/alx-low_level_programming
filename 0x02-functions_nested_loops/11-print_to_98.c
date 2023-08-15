93a1f87b41513a23df38443ac7579caff25a1efd
sirpheranme <sirpheranme@gmail.com>
2023-08-15 12:21:32 -0700

    11 print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,9 +6,11 @@
  */
 void print_to_98(int n)
 {
+    int i, j;
+
     if (n <= 98)
     {
-        for (int i = n; i <= 98; i++)
+        for (i = n; i <= 98; i++)
         {
             if (i != 98)
                 printf("%d, ", i);
@@ -18,7 +20,7 @@ void print_to_98(int n)
     }
     else if (n >= 98)
     {
-        for (int j = n; j >= 98; j--) 
+        for (j = n; j >= 98; j--) 
         {
             if (j != 98)
                 printf("%d, ", j);
