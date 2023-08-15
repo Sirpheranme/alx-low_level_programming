00a77cebca2a81a2fe37afd8cc961a4a8faa64f3
sirpheranme <sirpheranme@gmail.com>
2023-08-15 09:39:22 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
+
+void print_alphabet(void) {
+    char letter = 'a';
+    while (letter <= 'z') {
+        _putchar(letter);
+        letter++;
+    }
+    _putchar('\n');
+}
