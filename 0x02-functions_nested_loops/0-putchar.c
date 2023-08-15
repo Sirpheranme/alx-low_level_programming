59e99721dd89bc4b2759503a205ca8284708741c
sirpheranme <sirpheranme@gmail.com>
2023-08-15 08:58:07 -0700

    Putchar C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * main - Entry point
- * Return: 0
+ *
+ * Return: Always 0 (Success)
  */
- 
 int main(void)
 {
     printf("_putchar\n");
-    return 0;
+    return (0);
 }
 
